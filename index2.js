4c0cab9a87cf15fdcd5c4077a84224407242e4ec
Your Namejrpestana740 <jrpestana740@me.com>
2018-02-26 15:00:07 -0300

    ES6 Functions my job

---
+++ index2.js
@@ -0,0 +1,17 @@
+var sayHello1 = () => 'Hello';
+var sayHello = name => `Hello ${name}`;
+var printHello = name => {
+  console.log (`Hello ${name}`);
+};
+
+var getSiblings = n => ({ previus: n - 1, next: n + 1 });
+
+function sayHello() {
+  return "Hello";
+}
+
+console.log(sayhello1);
+console.log(sayhello('jose'));
+printHello('Pestana');
+console.log(getSiblins(10));
+
